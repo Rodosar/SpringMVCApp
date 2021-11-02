bd0f33354733e97384ac0843e8e1d163dcc0382f
Ruslan Kerimov <rodosar1@mail.ru>
2021-11-02 22:19:34 +0300

    Работа с аннотацией @RequestParam

---
+++ src/main/java/ru/webapp/controllers/FirstController.java
@@ -0,0 +1,30 @@
+package ru.webapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam("surname") String surname ){
+
+
+        System.out.println(name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage(){
+        return "first/goodbye";
+    }
+
+
+}
