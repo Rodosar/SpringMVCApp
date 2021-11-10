f704ab3e44b46dacd5c01efc058863bbf36a92fc
Ruslan Kerimov <rodosar1@mail.ru>
2021-11-10 21:59:22 +0300

    Сделал валидацию с помощью hibernate для создания и редактирования человека

--- src/main/java/ru/webapp/dao/PersonDAO.java
+++ src/main/java/ru/webapp/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
 
     {
         people=new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
-        people.add(new Person(++PEOPLE_COUNT,"Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT,"Tom", 24, "tom@mai;ru"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@mail.ru"));
     }
 
     public List<Person> index(){
@@ -27,5 +27,21 @@ public class PersonDAO {
 
     public Person show(int id){
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson){
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id){
+        people.removeIf(person -> person.getId()== id);
     }
 }
